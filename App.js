e1f6b13b727ca5305727340fe2708f490db92831
samridh <samridhpctebtech19cse@gmail.com>
2023-12-05 20:41:38 +0530

    updated

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 const express = require('express')
-const app=express();
-const port =3000;
+const app = express();
+const port = 3000;
 const mongoose = require("mongoose")
 require('dotenv').config();
 const cors = require('cors');
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("connected to mongo");
-    app.listen(()=>{
+    app.listen(() => {
         console.log("server started on port 3000");
     })
-}).catch(err=>{
+}).catch(err => {
     console.log(err);
 })
 app.use(express.json());
@@ -25,12 +25,12 @@ app.use(cors({
 
 app.use(User)
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello")
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("server is listening at port 3000")
 })
 
